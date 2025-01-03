014586dd4480789ff7e8deffe2211c9bbd9929c7
tulakethana <tulakethanapatel@gmail.com>
2025-01-03 10:08:13 +0530

    Add files via upload

---
+++ demo5.java
@@ -0,0 +1,10 @@
+public class demo5 {
+    public static void main(String[] args){
+        String username = "user001";
+        String pwd ="user_001";
+        while((username=="user001")&& (pwd=="user_001")){
+            System.out.println("hello user Happy New year");
+            break;
+        }
+    }
+}
