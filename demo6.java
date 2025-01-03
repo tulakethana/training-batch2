fb28d445e7df08dbfa530f931d986e362688f3da
tulakethana <tulakethanapatel@gmail.com>
2025-01-03 10:14:43 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,10 @@
+public class demo6 {
+    public static void main(String[] args){
+        String username = "Kethana";
+        String pwd ="Kethana@1305";
+        while((username=="Kethana")&& (pwd=="Kethana@1305")){
+            System.out.println("hello kethana welcome to mallareddy university");
+            break;
+        }
+    }
+}
