78ea6151150ce2d50fcf8e944a0fb2dae31cef8c
tulakethana <tulakethanapatel@gmail.com>
2025-01-08 10:01:44 +0530

    Add files via upload

---
+++ ps002.java
@@ -0,0 +1,11 @@
+public class ps002 {
+    public static void main(String[] args) {
+        // Number of multiples to print
+        int limit = 10; // You can change this to print more or fewer multiples
+
+        // Loop to print the multiples of 100
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(i * 100);
+        }
+    }
+}
