46634878752735bc8fe23622027fe4c5e83f0e83
tulakethana <tulakethanapatel@gmail.com>
2025-01-10 09:36:06 +0530

    Add files via upload

---
+++ Test01.java
@@ -0,0 +1,11 @@
+public class Test01{
+    public static void main(String[] args){
+        for (int i = 0; i < 10; i++) {
+            if(i!=5 && i!=7){
+                System.out.println(i);
+
+            }
+            
+        }
+    }
+}
