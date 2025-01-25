dc4b68c153dfab14d8ad678d3baa7ecc16501578
tulakethana <tulakethanapatel@gmail.com>
2025-01-25 10:14:39 +0530

    Add files via upload

---
+++ Demo029.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {"Alice", "Bob", "Charlie", "David", "Eve", 
+                          "Frank", "Grace", "Hannah", "Isaac", "Jack"};
+        
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
